05758201c9b6a34de7bd03f1258870267ab5e7f6
Panos Anastasiadis <anastapd@gmail.com>
2021-11-10 07:50:28 +0000

    Implemented event sync for query service

--- event-bus/index.js
+++ event-bus/index.js
@@ -6,8 +6,12 @@ const app = express();
 
 app.use(bodyParser.json())
 
+const events = []
+
 app.post("/events", (req, res) => {
     const event = req.body
+
+    events.push(event)
 
     axios.post("http://localhost:4000/events", event).catch(err => console.log(err))
     axios.post("http://localhost:4001/events", event).catch(err => console.log(err))
@@ -15,6 +19,10 @@ app.post("/events", (req, res) => {
     axios.post("http://localhost:4003/events", event).catch(err => console.log(err))
 
     res.send({ status: "OK" })
+})
+
+app.get("/events", (req, res) => {
+    res.send(events)
 })
 
 app.listen(4005, () => {
