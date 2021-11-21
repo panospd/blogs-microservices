676786524a31c4b1c8c3e0f02bef5d7f0a235fc4
panospd <anastapd@gmail.com>
2021-11-21 12:17:26 +0000

    refactored servics for deployments

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,30 +1,32 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const axios = require("axios")
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
 
 const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-const events = []
+const events = [];
 
-app.post("/events", (req, res) => {
-    const event = req.body
+app.post('/events', (req, res) => {
+  const event = req.body;
 
-    events.push(event)
+  events.push(event);
 
-    axios.post("http://localhost:4000/events", event).catch(err => console.log(err))
-    axios.post("http://localhost:4001/events", event).catch(err => console.log(err))
-    axios.post("http://localhost:4002/events", event).catch(err => console.log(err))
-    axios.post("http://localhost:4003/events", event).catch(err => console.log(err))
+  axios
+    .post('http://posts-clusterip-srv:4000/events', event)
+    .catch(err => console.log(err));
+  //axios.post("http://localhost:4001/events", event).catch(err => console.log(err))
+  //axios.post("http://localhost:4002/events", event).catch(err => console.log(err))
+  //axios.post("http://localhost:4003/events", event).catch(err => console.log(err))
 
-    res.send({ status: "OK" })
-})
+  res.send({ status: 'OK' });
+});
 
-app.get("/events", (req, res) => {
-    res.send(events)
-})
+app.get('/events', (req, res) => {
+  res.send(events);
+});
 
 app.listen(4005, () => {
-    console.log("Listening on 4005")
-})
+  console.log('Listening on 4005');
+});
