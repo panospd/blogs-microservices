b3ffa7f750acbcfaaaa83fe7ce475da123ccf8ec
panospd <anastapd@gmail.com>
2021-11-23 22:05:58 +0000

    created deployment files for query moderation and comments

--- comments/index.js
+++ comments/index.js
@@ -1,69 +1,68 @@
-const express = require("express")
-const cors = require("cors")
-const bodyParser = require("body-parser")
-const { randomBytes } = require("crypto")
-const axios = require("axios")
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+const axios = require('axios');
 
 const app = express();
-app.use(bodyParser.json())
-app.use(cors())
+app.use(bodyParser.json());
+app.use(cors());
 
-const commentsByPostId = {}
+const commentsByPostId = {};
 
-app.get("/posts/:id/comments", (req, res) => {
-    res.send(commentsByPostId[req.params.id] || [])
-})
+app.get('/posts/:id/comments', (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
 
-app.post("/posts/:id/comments", async (req, res) => {
-    const commentId = randomBytes(4).toString("hex")
-    const {content} = req.body
+app.post('/posts/:id/comments', async (req, res) => {
+  const commentId = randomBytes(4).toString('hex');
+  const { content } = req.body;
 
-    const comments = commentsByPostId[req.params.id] || []
+  const comments = commentsByPostId[req.params.id] || [];
 
-    const newComment = {
-        id: commentId, 
-        content,
-        status: "pending"
-    }
-    comments.push(newComment)
-    commentsByPostId[req.params.id] = comments
+  const newComment = {
+    id: commentId,
+    content,
+    status: 'pending',
+  };
+  comments.push(newComment);
+  commentsByPostId[req.params.id] = comments;
 
-    await axios.post("http://localhost:4005/events", {
-        type: "CommentCreated",
-        data: {
-            ...newComment,
-            postId: req.params.id
-        }
-    })
+  await axios.post('http://event-bus-srv:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      ...newComment,
+      postId: req.params.id,
+    },
+  });
 
-    res.status(201).send(comments)
-})
+  res.status(201).send(comments);
+});
 
-app.post("/events", async (req, res) => {
-    const { type, data } = req.body;
+app.post('/events', async (req, res) => {
+  const { type, data } = req.body;
 
-    if (type === "CommentModerated")
-    {
-        const { id, postId, status } = data
-        const comments = commentsByPostId[postId]
-        const comment = comments.find(c => c.id === id)
+  if (type === 'CommentModerated') {
+    const { id, postId, status } = data;
+    const comments = commentsByPostId[postId];
+    const comment = comments.find(c => c.id === id);
 
-        comment.status = status
+    comment.status = status;
 
-        console.log("From Comments", comment)
+    console.log('From Comments', comment);
 
-        await axios.post("http://localhost:4005/events", {
-            type: "CommentUpdated",
-            data: {
-                ...comment,
-                postId
-            }
-        })
-    }
+    await axios.post('http://event-bus-srv:4005/events', {
+      type: 'CommentUpdated',
+      data: {
+        ...comment,
+        postId,
+      },
+    });
+  }
 
-    res.send({})
-})
+  res.send({});
+});
 
 app.listen(4001, () => {
-    console.log("Listening on 4001")
-})
+  console.log('Listening on 4001');
+});
