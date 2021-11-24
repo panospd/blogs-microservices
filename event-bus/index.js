cf77dbc48bdbc447a6d2c9ab49f94e081a31ed82
panospd <anastapd@gmail.com>
2021-11-24 19:59:37 +0000

    added ingres and routing rules for all services

--- event-bus/index.js
+++ event-bus/index.js
@@ -19,13 +19,13 @@ app.post('/events', (req, res) => {
     .post('http://posts-clusterip-srv:4000/events', event)
     .catch(err => console.log(err));
   axios
-    .post('http://comments-srv:4001/events', event)
+    .post('http://comments-clusterip-srv:4001/events', event)
     .catch(err => console.log(err));
   axios
-    .post('http://query-srv:4002/events', event)
+    .post('http://query-clusterip-srv:4002/events', event)
     .catch(err => console.log(err));
   axios
-    .post('http://moderation-srv:4003/events', event)
+    .post('http://moderation-clusterip-srv:4003/events', event)
     .catch(err => console.log(err));
 
   res.send({ status: 'OK' });
